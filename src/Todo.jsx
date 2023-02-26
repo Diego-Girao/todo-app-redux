3c81040c81e87d97891717f19bee084b79223f3c
diegogirao <diegoramiro25@gmail.com>
2023-02-26 17:00:51 -0300

    Feat import darkMode and useSelector

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import crossIcon from "./images/icon-cross.svg"
 import checkIcon from "./images/icon-check.svg"
 
+import { useSelector } from "react-redux"
+import { selectDarkMode } from "./features/slices/themeSlice"
+
 function Todo() {
+	const darkMode = useSelector(selectDarkMode)
+
 	return (
 		<div className="todo_container">
 			<div className="circle">
 				<img src={checkIcon} alt="" />
 			</div>
 
-			<li className="todo">Example</li>
+			<li className={`todo ${!darkMode ? "whiteBg" : ""}`}>Example</li>
 
 			<img src={crossIcon} className="delete-icon" alt="" />
 		</div>
