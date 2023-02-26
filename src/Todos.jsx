f3dd3b867ecb018a5d8cc1c923af67f6363d0fe1
diegogirao <diegoramiro25@gmail.com>
2023-02-26 17:01:44 -0300

    Feat add darkMode and toggleTheme

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,16 +1,34 @@
 import React, { useRef } from "react"
 import moonIcon from "./images/icon-moon.svg"
-// import sunIcon from "./images/icon-sun.svg"
+import sunIcon from "./images/icon-sun.svg"
 import Todo from "./Todo"
+
+import { useDispatch, useSelector } from "react-redux"
+import { selectDarkMode, toggleTheme } from "./features/slices/themeSlice"
 
 function Todos() {
 	const inputRef = useRef()
+
+	const darkMode = useSelector(selectDarkMode)
+	const dispatch = useDispatch()
 
 	return (
 		<div className="todos">
 			<div className="todosHeader">
 				<h1>TODO</h1>
-				<img src={moonIcon} alt="icon toggle theme moon" />
+				{darkMode ? (
+					<img
+						src={sunIcon}
+						alt="icon toggle theme sun"
+						onClick={() => dispatch(toggleTheme())}
+					/>
+				) : (
+					<img
+						src={moonIcon}
+						alt="icon toggle theme moon"
+						onClick={() => dispatch(toggleTheme())}
+					/>
+				)}
 			</div>
 
 			<div className="input_container">
@@ -20,15 +38,16 @@ function Todos() {
 						type="text"
 						ref={inputRef}
 						placeholder="Create a new todo..."
+						className={!darkMode ? "whiteBg" : ""}
 					/>
 					<button type="submit" hidden></button>
 				</form>
 			</div>
 
-			<div className="todos_container">
+			<div className={`todos_container ${!darkMode ? "whiteBg" : ""}`}>
 				<Todo />
 
-				<div className="todos_footer">
+				<div className={`todos_footer ${!darkMode ? "whiteBg" : ""}`}>
 					<p>0 items left</p>
 
 					<div className="types">
