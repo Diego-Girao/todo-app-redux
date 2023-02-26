967eb4c5f3c2191908541ea9b931f5cf89759f43
diegogirao <diegoramiro25@gmail.com>
2023-02-26 17:02:21 -0300

    Fix changing names for todos and themes

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit"
+import todosReducer from "../features/slices/todosSlice"
+import themeReducer from "../features/slices/themeSlice"
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+	reducer: {
+		todos: todosReducer,
+		theme: themeReducer,
+	},
+})
