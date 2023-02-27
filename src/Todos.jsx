9d2859d4a89e35a3a8bc01a4403fcc5000910dd7
diegogirao <diegoramiro25@gmail.com>
2023-02-27 19:28:10 -0300

    Info add comments on function and variables

--- src/Todos.jsx
+++ src/Todos.jsx
@@ -3,8 +3,9 @@ import moonIcon from "./images/icon-moon.svg"
 import sunIcon from "./images/icon-sun.svg"
 import Todo from "./Todo"
 
-import { useDispatch, useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { selectDarkMode, toggleTheme } from "./features/slices/themeSlice"
+import { useDispatch } from "react-redux"
 
 import {
 	addTodo,
@@ -16,28 +17,35 @@ import {
 	selectShowTodos,
 	selectTodos,
 } from "./features/slices/todosSlice"
-
 import { showAllFunctions } from "./features/slices/todosSlice"
 import { showCompletedFunction } from "./features/slices/todosSlice"
 import { showActiveFunction } from "./features/slices/todosSlice"
 
 function Todos() {
+	// Referência para o input de adição de tarefas
 	const inputRef = useRef()
 
+	// Estado de tema escuro da aplicação
 	const darkMode = useSelector(selectDarkMode)
 	const dispatch = useDispatch()
 
+	// Seleciona todas as tarefas, tarefas completas e tarefas ativas da store
 	const todos = useSelector(selectTodos)
 	const completedTodos = useSelector(selectCompletedTodos)
 	const activeTodos = useSelector(selectActiveTodos)
 
+	// Estados para controlar o filtro de exibição de tarefas
 	const showTodos = useSelector(selectShowTodos)
 	const showActiveTodos = useSelector(selectShowActiveTodos)
 	const showCompletedTodos = useSelector(selectShowCompletedTodos)
 
+	// Variável para armazenar as tarefas que devem ser exibidas na lista
 	let todosToRender
+
+	// Contador de tarefas ativas
 	let activeTodosNumber = 0
 
+	// Função para adicionar nova tarefa à store
 	const submitTodo = (e) => {
 		e.preventDefault()
 
@@ -54,6 +62,7 @@ function Todos() {
 		inputRef.current.value = ""
 	}
 
+	// Funções para atualizar o estado de filtro de exibição de tarefas
 	const showCompletedHandler = () => {
 		dispatch(showCompletedFunction())
 	}
@@ -66,10 +75,12 @@ function Todos() {
 		dispatch(showActiveFunction())
 	}
 
+	// Função para limpar tarefas completas da store
 	const clearCompletedHandler = () => {
 		dispatch(clearCompleted())
 	}
 
+	// Define qual conjunto de tarefas deve ser exibido com base no estado de filtro
 	if (showActiveTodos) {
 		todosToRender = activeTodos
 	} else if (showCompletedTodos) {
@@ -78,6 +89,7 @@ function Todos() {
 		todosToRender = todos
 	}
 
+	// Conta o número de tarefas ativas
 	todos?.forEach((todo) => {
 		if (!todo.completed) {
 			activeTodosNumber++
@@ -86,8 +98,10 @@ function Todos() {
 
 	return (
 		<div className="todos">
+			{/* Cabeçalho da aplicação */}
 			<div className="todosHeader">
 				<h1>TODO</h1>
+				{/* Botão de alternância de tema */}
 				{darkMode ? (
 					<img
 						src={sunIcon}
@@ -121,6 +135,7 @@ function Todos() {
 					<Todo
 						content={todo.content}
 						key={todo.id}
+						id={todo.id}
 						completed={todo.completed}
 					/>
 				))}
